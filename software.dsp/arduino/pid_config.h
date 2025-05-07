f8e5150a726c6138c7b261af0ad8c5581ede25c9
Eduardo Amaral <eduamaralmp@gmail.com>
2025-05-06 22:44:12 -0300

    feature(dev)-config ambiente de desenvolvimento (#4)
    * Update README.md
    
    * Update README.md
    
    * ambiente de trabalho

---
+++ software.dsp/arduino/pid_config.h
@@ -0,0 +1,10 @@
+' Controle PID (Proporcional-Integral-Derivativo)
+Este sistema PID é essencial para:
+
+Movimentos suaves e precisos
+
+Compensação de cargas variáveis
+
+Correção de folgas mecânicas
+
+Estabilidade contra perturbações '
